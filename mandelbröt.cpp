2d9671dea4d0144f886d8c589a485e9e8f46fecb
Fabian Gruber <fabian.gruber@inria.fr>
2019-03-03 01:49:15 +0100

    Many bug fixes & tweaks.
    * Fixed a bug in the divergence check in `mandelbrot_escape_time` (results in less artifacts & smother images)
    * Parallelized loop over image with OpenMP.
    * Changed color blending to work with different max_iterations.
    * Change max_iterations over time to get an animation effect.
    * Calculate in higher precision than window resolution. SDL takes care of scaling when the window size changes.
    * Window title now shows the current number of max_iterations.
    * Start with maximized window

--- mandelbröt.cpp
+++ mandelbröt.cpp
@@ -9,10 +9,7 @@
 #include <bits/unique_ptr.h>
 #include <cstring>
 
-struct Main {
-};
-
-using Real = float;
+using Real = double;
 
 // Precise method, which guarantees v = v1 when t = 1.
 Real lerp(Real v0, Real v1, Real t) {
@@ -23,7 +20,7 @@ Real scale(Real value, Real src_min, Real src_max, Real dst_min, Real dst_max) {
   return dst_min + ((dst_max - dst_min) * value) / (src_max - src_min);
 }
 
-unsigned mandelbrot_escape_time(unsigned Px, unsigned Py, unsigned max_x, unsigned max_y) {
+unsigned mandelbrot_escape_time(int max_iterations, unsigned Px, unsigned Py, unsigned max_x, unsigned max_y) {
   const Real mandelbrot_min_x = -2.5;
   const Real mandelbrot_max_x = +1.0;
 
@@ -38,20 +35,16 @@ unsigned mandelbrot_escape_time(unsigned Px, unsigned Py, unsigned max_x, unsign
   Real x = 0.0;
   Real y = 0.0;
 
-  unsigned max_iteration = 1000;
+  int iteration = 0;
 
-  for (int iteration = 0; iteration < max_iteration; iteration++) {
+  for (; (iteration < max_iterations) and (x*x + y*y <= 2*2); iteration++) {
     const Real xtemp = x*x - y*y + x0;
 
-    if (xtemp > (x*x + y*y <= 2*2)) {
-      return iteration;
-    }
-
-    y = 2*x*y + y0;
+    y = 2 * x * y + y0;
     x = xtemp;
   }
 
-  return max_iteration;
+  return iteration;
 }
 
 struct Colour {
@@ -83,6 +76,8 @@ struct Colour {
 
 inline static const Colour WHITE = Colour::rgb(255, 255, 255);
 inline static const Colour BLACK = Colour::rgb(0, 0, 0);
+inline static const Colour RED   = Colour::rgb(255, 0, 0);
+inline static const Colour GREEN = Colour::rgb(0, 255, 0);
 
 struct Image {
   Image(uint32_t width, uint32_t height) : _width{width}, _height{height} {
@@ -93,10 +88,10 @@ struct Image {
     assert(x < _width);
     assert(y < _height);
 
-    return pixels[y * _height + x];
+    return pixels[y * _width + x];
   }
 
-  void clear(Colour C = BLACK) {
+  void clear(Colour C) {
     std::fill(pixels.get(), pixels.get() + width() * height(), C);
   }
 
@@ -110,28 +105,30 @@ private:
 };
 
 struct Color_Pallette {
-  static constexpr inline unsigned BAD  =    0;
-  static constexpr inline unsigned GOOD =  500;
-  static constexpr inline unsigned BEST = 1000;
+  Colour operator()(unsigned max_N, unsigned N) const {
+    N = std::min(N, max_N);
 
-  Colour operator()(unsigned N) const {
-    N = std::min(N, BEST);
+    Real score = Real(N) / max_N;
 
     const Colour C_BAD  = Colour::rgb(255, 0, 0);
     const Colour C_OK   = Colour::rgb(0, 255, 0);
     const Colour C_GOOD = Colour::rgb(0, 0, 255);
     const Colour C_BEST = Colour::rgb(255, 0, 255);
 
-    if (N <= BAD) {
-      return Colour::lerp(C_BAD, C_OK, scale(N, 0, BAD, 0, 1));
+    const Real BAD  = 0.25;
+    const Real GOOD = 0.75;
+    const Real BEST = 0.90;
+
+    if (score <= BAD) {
+      return Colour::lerp(C_BAD, C_OK, scale(score, 0, BAD, 0, 1));
     }
 
-    if (N <= GOOD) {
-      return Colour::lerp(C_OK, C_GOOD, scale(N, BAD, GOOD, 0, 1));
+    if (score <= GOOD) {
+      return Colour::lerp(C_OK, C_GOOD, scale(score, BAD, GOOD, 0, 1));
     }
 
-    if (N <= BEST) {
-      return Colour::lerp(C_GOOD, C_BEST, scale(N, GOOD, BEST, 0, 1));
+    if (score < BEST) {
+      return Colour::lerp(C_GOOD, C_BEST, scale(score, GOOD, BEST, 0, 1));
     }
 
     return BLACK;
@@ -147,10 +144,10 @@ int main(int argc, const char **argv) {
   const unsigned WIN_WIDTH  = 640;
   const unsigned WIN_HEIGHT = 480;
 
-  const uint32_t window_flags = SDL_WINDOW_RESIZABLE;
+  const uint32_t window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED;
 
   SDL_Window *window = SDL_CreateWindow(
-    "Rendering to a texture!",
+    "Mandelbröt!",
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
     WIN_WIDTH,
@@ -167,23 +164,24 @@ int main(int argc, const char **argv) {
     renderer_flags
   );
 
-  const int texture_width  = WIN_WIDTH;
-  const int texture_height = WIN_HEIGHT;
+  const unsigned TEXTURE_WIDTH  = 1 << 12;
+  const unsigned TEXTURE_HEIGHT = 1 << 12;
 
-  //Make a target texture to render too
   SDL_Texture *texture = SDL_CreateTexture(
     renderer,
     SDL_PIXELFORMAT_RGBA8888,
     SDL_TEXTUREACCESS_TARGET,
-    texture_width, texture_height
+    TEXTURE_WIDTH, TEXTURE_HEIGHT
   );
 
-  Image pixels{texture_width, texture_height};
-  pixels.clear();
+  Image pixels{TEXTURE_WIDTH, TEXTURE_HEIGHT};
+  pixels.clear(BLACK);
 
   Color_Pallette pallette;
 
   const uint64_t performance_frequency = SDL_GetPerformanceFrequency();
+
+  unsigned max_iterations = 0;
 
   SDL_Event event;
   bool running = true;
@@ -210,25 +208,33 @@ int main(int argc, const char **argv) {
 
     if (!running) { break; }
 
+    if (max_iterations > 1000) { max_iterations = 0; }
+    max_iterations++;
+
+    std::string title = "Mandelbröt! N=" + std::to_string(max_iterations);
+
+    SDL_SetWindowTitle(window, title.c_str());
+
     /// splat down some random pixels
-    pixels.clear();
+    pixels.clear(BLACK);
 
     const unsigned MIN_X = 0;
-    const unsigned MAX_X = texture_width;
+    const unsigned MAX_X = TEXTURE_WIDTH;
 
     const unsigned MIN_Y = 0;
-    const unsigned MAX_Y = texture_height;
+    const unsigned MAX_Y = TEXTURE_HEIGHT;
 
-    for (unsigned y = MIN_Y; y < MAX_Y; y++) {
-      for (unsigned x = MIN_X; x < MAX_X; x++) {
-        const unsigned N = mandelbrot_escape_time(x, y, texture_width, texture_height);
+    #pragma omp parallel for collapse(2)
+    for (unsigned x = MIN_X; x < MAX_X; x++) {
+      for (unsigned y = MIN_Y; y < MAX_Y; y++) {
+        const unsigned N = mandelbrot_escape_time(max_iterations, x, y, MAX_X, MAX_Y);
 
-        pixels(x, y) = pallette(N);
+        pixels(x, y) = pallette(max_iterations, N);
       }
     }
 
     /// pitch ... the number of bytes in a row of pixel data, including padding between lines
-    const int pitch = texture_width * 4;
+    const int pitch = TEXTURE_WIDTH * 4;
 
     SDL_UpdateTexture(
       texture,
@@ -245,13 +251,13 @@ int main(int argc, const char **argv) {
       const double   seconds       = (end - start) / double(performance_frequency);
       const double   milli_seconds = seconds * 1000;
 
-      const unsigned frames_per_second  = 5;
+      const unsigned frames_per_second  = 30;
       const double   target_frame_time = (1.0 / frames_per_second) * 1000;
 
       if (milli_seconds < target_frame_time) {
         SDL_Delay(target_frame_time - milli_seconds);
       } else if (milli_seconds > target_frame_time) {
-        std::cout << "Dropped a frame! Frame time: " << milli_seconds << "ms" << std::endl;
+        std::cout << "Dropped a frame! Frame time: " << milli_seconds << "ms (max_iterations=" << max_iterations << ")" << std::endl;
       }
     }
   }
